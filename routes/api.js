2063144701670feec019b3b830c00c1ef9f59129
timbeurskens <timbeurskens@keybase.io>
2016-05-19 19:49:07 +0200

    add geonames library

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var geonames = require('../lib/geonames');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/countries', function(req, res, next){
+  geonames.countries().then(function(httpres){
+    res.send(httpres);
+  }, function(error){
+    next(error);
+  });
 });
 
 module.exports = router;
