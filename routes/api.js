c1fdbee1f81c2f6bd9e69b8754f53d4f065a2135
timbeurskens <timbeurskens@keybase.io>
2016-05-19 21:25:05 +0200

    add sorting methods

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var geonames = require('../lib/geonames');
+var countrygraphs = require('../lib/countrygraphs');
 var router = express.Router();
 
 /* GET users listing. */
@@ -10,6 +11,24 @@ router.get('/', function(req, res, next) {
 router.get('/countries', function(req, res, next){
   geonames.countries().then(function(httpres){
     res.send(httpres);
+  }, function(error){
+    next(error);
+  });
+});
+
+router.get('/sorted', function(req, res, next){
+  geonames.countries().then(function(apiresult){
+    console.log("result");
+    console.log(apiresult);
+    var countries = apiresult.geonames;
+    var nlIndex = countrygraphs.getCountryCodeIndex("NL", countries);
+    var compareFunction = countrygraphs.getCountryCompareFunction(nlIndex, countries);
+    countries.sort(compareFunction);
+    console.log(countries);
+    var nameList = countrygraphs.getNameDistanceList(nlIndex,countries);
+    console.log(nameList);
+    console.log(nameList.length);
+    res.send(nameList);
   }, function(error){
     next(error);
   });
